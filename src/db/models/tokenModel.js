ec7d1132af190299705e9d37e6e914dc099d02f5
Keli Booster <kbooster17@gmail.com>
2022-04-08 14:32:37 +0000

    add route to get emails list

--- src/db/models/tokenModel.js
+++ src/db/models/tokenModel.js
@@ -1,25 +1,19 @@
 const mongoose = require("mongoose");
 
 const tokenSchema = new mongoose.Schema({
-  credentials: {
-    access_token: {
-      type: String,
-      required: true,
-    },
-    refresh_token: {
-      type: String,
-      required: true,
-    },
-    scope: {
-      type: String,
-      required: true,
-    },
-    token_type: {
-      type: String,
-    },
-    expiry_date: {
-      type: Number,
-    },
+  access_token: {
+    type: String,
+    required: true,
+  },
+  scope: {
+    type: String,
+    required: true,
+  },
+  token_type: {
+    type: String,
+  },
+  expiry_date: {
+    type: Number,
   },
 });
 
--- src/routers/tokenRouter.js
+++ src/routers/tokenRouter.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const authorize = require("../db/authorize");
+const Token = require("../db/models/tokenModel");
 const router = new express.Router();
 const SCOPES = ["https://mail.google.com/"];
+const axios = require("axios");
 
 router.get("/v1/token/:clientSecret/:clientId", async (req, res) => {
   const { clientSecret, clientId } = req.params;
-  console.log("secreate", clientSecret);
-  console.log(clientId);
   const redirect_uris = ["http://localhost:3001/auth"];
 
   const oAuth2Client = authorize({
@@ -18,11 +18,45 @@ router.get("/v1/token/:clientSecret/:clientId", async (req, res) => {
     access_type: "offline",
     scope: SCOPES,
   });
-  res.send({ LoginToken: authUrl });
+  res.send({ authUrl });
 });
 
 router.get("/auth", async (req, res) => {
-  res.status(201).send("Successfully generated refresh token");
+  try {
+    const { code } = req.query;
+    const token = await oAuth2Client.getToken(code);
+    console.log(token.tokens);
+    const savedToken = await Token(token.tokens);
+    savedToken.save();
+    const { access_token, scope, token_type, expiry_date } = token.tokens;
+    res.status(201).send({ access_token, scope, token_type });
+    await oAuth2Client.setCredentials(token);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get("/v1/mails", async (req, res) => {
+  const curToken = await Token.findById("6250465e86c219cdcda1bab4");
+  if (!curToken.access_token) {
+    return res.status(400).send("unable to get access token from the database");
+  }
+
+  const config = {
+    method: "get",
+    url: "https://gmail.googleapis.com/gmail/v1/users/me/messages",
+    headers: {
+      Authorization: `Bearer ${curToken.access_token}`,
+    },
+  };
+
+  axios(config)
+    .then(function (response) {
+      return res.status(200).send(response.data);
+    })
+    .catch(function (error) {
+      res.status(400).send({ error });
+    });
 });
 
 module.exports = router;
